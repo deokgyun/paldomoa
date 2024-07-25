d6fa78e1f269708af3fbebd2b7247147bad67139
deokgyun <micron6594@naver.com>
2024-07-25 20:39:02 +0900

    init frontend

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Header from '@/components/Header'
-import Footer from '@/components/Footer'
+import Footer from '@/components/footer'
 import React from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -16,17 +16,17 @@ export const metadata: Metadata = {
 }
 
 export default function RootLayout({
-  children,
-}: Readonly<{
+                                     children,
+                                   }: Readonly<{
   children: React.ReactNode
 }>) {
   return (
     <html lang="en" className={inter.className}>
-      <body className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-grow">{children}</main>
-        <Footer />
-      </body>
+    <body className="flex flex-col min-h-screen">
+    <Header />
+    <main className="flex-grow">{children}</main>
+    <Footer />
+    </body>
     </html>
   )
 }
